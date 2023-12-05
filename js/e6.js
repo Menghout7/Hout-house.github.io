5c089fa6328253271f4fe74cbe9ab30dd5917b74
Menghout7 <152957705+Menghout7@users.noreply.github.com>
2023-12-05 21:25:12 +1400

    Add files via upload

---
+++ js/e6.js
@@ -0,0 +1,6 @@
+var v1="public variable 1";
+let l1="public variable 2";
+
+var v1= 0;
+let l1=0;
+alert("Write by external script! សួរស្ដី-Scriptក្រៅweb pageគឺក្នុងfile e6.js");
---
+++ js/js.js
@@ -0,0 +1,19 @@
+javascript ជាភាសាធ្វើអោយព័ត៍មានលើweb page រឺ​ រចនាសម្ព័ន្ធweb page ផ្លាស់ប្ដូរពេលេវាកំពុងដំណើរដោយប្រើធនធាន(resoures)
+របស់ម៉ាស៉ីនអ្នកទស្សនា។
+
+dynamic web page រឺ dynamic web site វាអាចផ្លាស់ប្ដូរពេលកំពុងដំណើរការ។
+1.client side រឺ front-end
+2.server side រឺ back-end ប្រើ Server's resources(hardware, software, file, services)s។​ 
+
+របៀបសរសេរ
+ inline scriptសរសេរscriptក្នុងធាតុ
+ <... on_event='javascript code'></>
+ internal script សរសេរscriptក្នុងWeb page
+ <script>javascript code</script>
+ alert('') ជូនដំណីង
+external script សរសេរscriptក្រៅWeb page គឺ​សរសេរក្នុងfile javascript។
+1.បង្កើត fileដែលមានកន្ទុយ .js
+2.ក្នុងweb page ប្រើscriptដើម្បីភ្ចាប់ទៅfile javascript
+    <script type="text/javascript" src="adress file javascript">
+
+    </script>
